feb075e14f96d45137e5416ce4479d1e3e9c6c1b
Dzulfanni <belajarayok866@gmail.com>
2020-09-23 13:38:27 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+const asalSantri = {
+    "Sidik": "Lampung",
+    "Wandi": "Prabumulih",
+    "Rahmad": "Medan",
+    "Usamah": "Ceribon",
+    "Syofyan": "Purwokerto",
+    "Aldi": "Kendari",
+    "Kardi": "Lombok"
+}
+const{Sidik,Wandi,Rahmad,Usamah,...rest}=asalSantri
+console.log(Sidik,Wandi,Rahmad,Usamah,rest);
